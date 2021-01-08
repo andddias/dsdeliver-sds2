15b7002e27fff969e9e94a0f9465534c65dca539
Andre Luiz <and.ddias@gmail.com>
2021-01-07 23:15:11 -0300

    Exemplos de controle de estados

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
-import Hello from './Hello'
+import Counter from './Counter'
 
 function App() {
   return (
     <div>
-      <Hello message="Washington" />
-      <Hello message="Nélio" />
-      <Hello message="SDS2" />
-      <Hello message="DevSuperior" />
+      <Counter />
     </div>
   );
 }
